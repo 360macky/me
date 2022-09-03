a99563032e49d2796f7af655c951ae13334b1cc4
Marcelo Arias <hello@marceloarias.com>
2022-09-02 22:58:09 -0500

    Remove base url from language routing

--- lang/core/language.js
+++ lang/core/language.js
@@ -4,8 +4,8 @@ document.cookie = 'preferredLanguage=' + languageCode;
 let languageEs = {};
 let languageEn = {};
 
-const SPANISH_LANGUAGE = 'https://portfolio.marceloarias.com/lang/es.json';
-const ENGLISH_LANGUAGE = 'https://portfolio.marceloarias.com/lang/en.json';
+const SPANISH_LANGUAGE = '/lang/es.json';
+const ENGLISH_LANGUAGE = '/lang/en.json';
 
 const ENGLISH_PROJECTS = {
     0: {
@@ -13,6 +13,7 @@ const ENGLISH_PROJECTS = {
         description: 'Ecommerce website powered by Firebase and integrated with Stripe',
         tags: ['React', 'Firebase', 'Material Icons', 'Stripe', 'React Router'],
         url_repo: 'https://github.com/360macky/Saturn',
+        url_learn: 'https://github.com/360macky/Saturn',
         url_deploy: 'https://saturn-32b13.web.app/',
         image: 'https://portfolio.marceloarias.com/images/projects/saturn.webp',
     },
@@ -21,6 +22,7 @@ const ENGLISH_PROJECTS = {
         description: 'SpaceX Capsules Finder with information like landings, status, type and more',
         tags: ['SpaceX API', 'React', 'NodeJS', 'SpaceX'],
         url_repo: 'https://github.com/360macky/spacex-capsules-search',
+        url_learn: 'https://github.com/360macky/spacex-capsules-search',
         url_deploy: 'https://1spacex.com/',
         image: 'https://portfolio.marceloarias.com/images/projects/spacex-search.webp',
     },
@@ -29,6 +31,7 @@ const ENGLISH_PROJECTS = {
         description: 'A simple photo editing web app',
         tags: ['JavaScript', 'Filters.js'],
         url_repo: 'https://github.com/360macky/miniedit',
+        url_learn: 'https://github.com/360macky/miniedit',
         url_deploy: 'https://miniedit.me/',
         image: 'https://portfolio.marceloarias.com/images/projects/miniedit.webp',
     },
@@ -37,6 +40,7 @@ const ENGLISH_PROJECTS = {
         description: 'Search and know more about your favorite movies instantly',
         tags: ['React', 'NodeJS', 'Async-await', 'The Movie Database', 'JSON', 'JavaScript'],
         url_repo: 'https://github.com/360macky/ReactFlix',
+        url_learn: 'https://github.com/360macky/ReactFlix',
         url_deploy: 'https://reactflix.vercel.app/',
         image: 'https://portfolio.marceloarias.com/images/projects/reactflix.webp',
     },
@@ -45,6 +49,7 @@ const ENGLISH_PROJECTS = {
         description: 'The first Bank-to-bank transfer simulator in Peru',
         tags: ['React', 'React Native', 'Expo', 'React Navigation'],
         url_repo: 'https://github.com/360macky/hackbank/',
+        url_learn: 'https://github.com/360macky/hackbank/',
         url_deploy: 'https://hackbank.app/',
         image: 'https://portfolio.marceloarias.com/images/projects/hackbank.webp',
     },
@@ -54,6 +59,7 @@ const ENGLISH_PROJECTS = {
             'A Chrome extension that improves Platzi Exams with keyboard shortcuts, an integrated whiteboard and Zen/SSH mode',
         tags: ['Chrome', 'JavaScript', 'Extension', 'DOM'],
         url_repo: 'https://github.com/360macky/platzikey',
+        url_learn: 'https://github.com/360macky/platzikey',
         url_deploy: 'https://github.com/360macky/platzikey',
         image: 'https://portfolio.marceloarias.com/images/projects/platzikey.webp',
     },
@@ -65,6 +71,7 @@ const SPANISH_PROJECTS = {
         description: 'Sitio web de comercio electrónico alojado en Firebase e integrado con Stripe',
         tags: ['React', 'Firebase', 'Material Icons', 'Stripe', 'React Router'],
         url_repo: 'https://github.com/360macky/Saturn',
+        url_learn: 'https://github.com/360macky/Saturn',
         url_deploy: 'https://saturn-32b13.web.app/',
         image: 'https://portfolio.marceloarias.com/images/projects/saturn.webp',
     },
@@ -73,6 +80,7 @@ const SPANISH_PROJECTS = {
         description: 'Buscador de cápsulas SpaceX con información como aterrizajes, estado, tipo y más',
         tags: ['SpaceX API', 'React', 'NodeJS', 'SpaceX'],
         url_repo: 'https://github.com/360macky/spacex-capsules-search',
+        url_learn: 'https://github.com/360macky/spacex-capsules-search',
         url_deploy: 'https://1spacex.com/',
         image: 'https://portfolio.marceloarias.com/images/projects/spacex-search.webp',
     },
@@ -81,6 +89,7 @@ const SPANISH_PROJECTS = {
         description: 'Una aplicación web simple de edición de fotografías.',
         tags: ['JavaScript', 'Filters.js'],
         url_repo: 'https://github.com/360macky/miniedit',
+        url_learn: 'https://github.com/360macky/miniedit',
         url_deploy: 'https://miniedit.me/',
         image: 'https://portfolio.marceloarias.com/images/projects/miniedit.webp',
     },
@@ -89,6 +98,7 @@ const SPANISH_PROJECTS = {
         description: 'Busca y conoce más sobre tus películas favoritas',
         tags: ['React', 'NodeJS', 'Async-await', 'The Movie Database', 'JSON'],
         url_repo: 'https://github.com/360macky/ReactFlix',
+        url_learn: 'https://github.com/360macky/ReactFlix',
         url_deploy: 'https://reactflix.vercel.app/',
         image: 'https://portfolio.marceloarias.com/images/projects/reactflix.webp',
     },
@@ -97,6 +107,7 @@ const SPANISH_PROJECTS = {
         description: 'El primer simulador de transferencia de banco-a-banco en Perú',
         tags: ['React', 'React Native', 'Expo', 'React Navigation'],
         url_repo: 'https://github.com/360macky/hackbank/',
+        url_learn: 'https://github.com/360macky/hackbank/',
         url_deploy: 'https://hackbank.app/',
         image: 'https://portfolio.marceloarias.com/images/projects/hackbank.webp',
     },
@@ -106,6 +117,7 @@ const SPANISH_PROJECTS = {
             'Una extensión que mejora la experiencia de los exámenes de Platzi con atajos de teclado, una pizarra integrada y modo Zen/SSH',
         tags: ['Chrome', 'JavaScript', 'Extension', 'DOM'],
         url_repo: 'https://github.com/360macky/platzikey',
+        url_learn: 'https://github.com/360macky/platzikey',
         url_deploy: 'https://github.com/360macky/platzikey',
         image: 'https://portfolio.marceloarias.com/images/projects/platzikey.webp',
     },
@@ -121,7 +133,6 @@ function renderLanguageContent(language) {
     // Projects
     document.getElementById('projectsTitle').textContent = language.projects.title;
     document.getElementById('projectsDescription').textContent = language.projects.description;
-    document.getElementById('search').placeholder = language.projects.projectInput;
     document.getElementById('show-all').textContent = language.projects.moreProjectsButton;
     document.getElementById('show-all-text').textContent = language.projects.textMoreProjects;
 
@@ -169,6 +180,15 @@ function renderLanguageContent(language) {
     ) {
         document.getElementsByClassName('lookCode')[buttonIndex].textContent =
             language.projects.projectCard.lookCode;
+    }
+
+    for (
+        let buttonIndex = 0;
+        buttonIndex < document.getElementsByClassName('learnMore').length;
+        buttonIndex++
+    ) {
+        document.getElementsByClassName('learnMore')[buttonIndex].textContent =
+            language.projects.projectCard.learnMore;
     }
 
     for (
